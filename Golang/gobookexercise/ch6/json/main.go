718631d69931ca966d7cb5aecf3ead00233031e7
zachha <zachha@tencent.com>
2021-11-17 10:58:20 +0800

    databse

--- Golang/gobookexercise/ch6/json/main.go
+++ Golang/gobookexercise/ch6/json/main.go
@@ -10,13 +10,17 @@ type Astruct struct {
 }
 
 func main() {
-	// s := "{\"name\":\"name\",\"type\":\"type\"}"
-	amap := make(map[string]string)
-	// var astruct Astruct
-	// _ = json.Unmarshal([]byte(s), &amap)
-	amap["name"] = "name"
-	amap["type"] = "type"
-	fmt.Printf("unmarshal: %v\n", amap)
-	data, _ := json.Marshal(amap)
-	fmt.Printf("marshal: %s\n", string(data))
+	ss := []string{"a", "b", "c", "d", "c", "b", "a"}
+	j, _ := json.Marshal(ss)
+	result := make([]string, 0)
+	_ = json.Unmarshal([]byte(j), &result)
+	fmt.Printf("result:%v", result)
+	// amap := make(map[string]string)
+	// // var astruct Astruct
+	// // _ = json.Unmarshal([]byte(s), &amap)
+	// amap["name"] = "name"
+	// amap["type"] = "type"
+	// fmt.Printf("unmarshal: %v\n", amap)
+	// data, _ := json.Marshal(amap)
+	// fmt.Printf("marshal: %s\n", string(data))
 }
